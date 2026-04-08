c4f4e84d52222843b2e10c2edf6167e6f40e4c63
dhyana19 <dhyana1907@gmail.com>
2026-04-08 17:16:56 -0400

    Remove debug and app files

--- debug_short_run.py
+++
@@ -1,9 +0,0 @@
-import sys, os
-print('Starting short debug training run (N_TRAIN=20, EPOCHS=1)')
-# Ensure src is importable
-sys.path.insert(0, os.path.join(os.getcwd(), 'src'))
-import train_snn
-train_snn.N_TRAIN = 20
-train_snn.EPOCHS = 1
-train_snn.train_snn()
-print('Debug run complete')
